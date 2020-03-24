7a6cfc80879c1aba180271be393d148d8aa4504f
bretdabel <bdabel@harriscomputer.com>
2020-03-24 12:49:02 -0700

    fix attribute

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -10,10 +10,9 @@ using WishList.Models;
 
 namespace WishList.Controllers
 {
-    public class AccountController : Controller
+    [Authorize]
+    public class AccountController : Controller 
     {
-        public Attribute Authorize;
-
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser>  _signInManager;
 
@@ -23,88 +22,6 @@ namespace WishList.Controllers
             _signInManager = signInManager;
         }
 
-
-
-
-        // GET: Account
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Account/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Account/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Account/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Account/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Account/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Account/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Account/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
