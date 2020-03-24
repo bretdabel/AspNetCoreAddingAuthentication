762d9a57d350b0ec8475e9aed1fd7bc69841772f
bretdabel <bdabel@harriscomputer.com>
2020-03-24 16:14:43 -0700

    updated

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+
 using Microsoft.AspNetCore.Identity;
 using WishList.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WishList.Controllers
 {
